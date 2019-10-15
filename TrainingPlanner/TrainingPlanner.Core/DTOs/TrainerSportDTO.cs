888bcc8ee33d1746536b427332a9ce6269e3cdc5
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-10-15 23:06:04 +0200

    DTOs change

--- TrainingPlanner/TrainingPlanner.Core/DTOs/TrainerSportDTO.cs
+++ TrainingPlanner/TrainingPlanner.Core/DTOs/TrainerSportDTO.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public int TrainerId { get; set; }
-        public TrainerDTO Trainer { get; set; }
         public int SportId { get; set; }
         public SportDTO Sport { get; set; }
 
